a42cfa003b002408bc029903282c8cbf80bfe1c8
mpoapostolis <mpoapostolis@gmail.com>
2025-04-05 23:59:51 +0300

    Add under development badge to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,6 +49,23 @@ export function Home() {
 
       <Header />
 
+      {/* Under Construction Badge */}
+      <div className="fixed top-20 right-4 z-50 bg-yellow-600 text-white px-4 py-2 rounded-lg shadow-lg transform rotate-3 border-2 border-yellow-400 flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <span className="font-bold">Under Development</span>
+      </div>
+
       {/* Main Content */}
       <main className="relative z-10">
         <Hero />
